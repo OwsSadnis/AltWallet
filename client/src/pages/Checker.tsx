27f11d3e4c88ccc6d49652a2bc775367a58cf673
Dhanis <dzaakydhanis1@gmail.com>
2026-06-01 06:35:26 +0700

    fix: move sample scan card to top-right fixed position below navbar

--- client/src/pages/Checker.tsx
+++ client/src/pages/Checker.tsx
@@ -223,7 +223,7 @@ export default function Checker() {
         <>
           <div
             className="hidden md:block"
-            style={{ position: "absolute", top: 0, right: 0, width: 260, zIndex: 10 }}
+            style={{ position: "fixed", top: 70, right: 24, width: 220, zIndex: 10 }}
           >
             <Reveal delay={160}>
               <SampleScanCard onScan={startMultiScan} />
