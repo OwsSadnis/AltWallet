a13f93e564d7b6ecd377659faeff4e5c1b57da66
Dhanis <dzaakydhanis1@gmail.com>
2026-06-01 17:03:04 +0700

    fix: sample scan card right:0, notification z-index 999

--- client/src/pages/Checker.tsx
+++ client/src/pages/Checker.tsx
@@ -223,7 +223,7 @@ export default function Checker() {
         <>
           <div
             className="hidden md:block"
-            style={{ position: "fixed", top: 70, right: 32, width: 220, zIndex: 10 }}
+            style={{ position: "fixed", top: 70, right: 0, width: 160, zIndex: 10 }}
           >
             <Reveal delay={160}>
               <SampleScanCard onScan={startMultiScan} />
